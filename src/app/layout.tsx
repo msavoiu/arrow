08bd672b49e1f183cce1f02459dbc31cdb47d49b
msavoiu <savoiumadeline@gmail.com>
2025-03-13 12:00:37 -0700

    removed mobile hamburger menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,38 +40,8 @@ export default function RootLayout({
                     Profile
                   </Link>
                 </div>
-                
-                Mobile menu button
-                <div className="md:hidden">
-                  <button className="mobile-menu-button">
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                    </svg>
-                  </button>
-                </div>
               </div>
-              
-              {/* Mobile menu - hidden by default */}
-              <div className="mobile-menu hidden md:hidden mt-2">
-                <Link href="/map" className="block py-2 px-4 text-sm hover:bg-gray-700">
-                  Map
-                </Link>
-                <Link href="/match" className="block py-2 px-4 text-sm hover:bg-gray-700">
-                  Match
-                </Link>
-                <Link href="/event-finder" className="block py-2 px-4 text-sm hover:bg-gray-700">
-                  Event Finder
-                </Link>
-                <Link href="/login" className="block py-2 px-4 text-sm hover:bg-gray-700">
-                  Login
-                </Link>
-                <Link href="/register" className="block py-2 px-4 text-sm hover:bg-gray-700">
-                  Register
-                </Link>
-                <Link href="/profile" className="block py-2 px-4 text-sm hover:bg-gray-700">
-                  Profile
-                </Link>
-              </div>
+
             </nav>
           </header>
           
